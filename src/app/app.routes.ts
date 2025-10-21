f3e469eef518f82e99456a90033ca2a86f137bfd
Carlota Cetina <carlotika26@gmail.com>
2025-10-21 18:50:30 +0200

    Add header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: 'header', component: Header },
+      { path: 'footer', component: Footer },
+      { path: '', component: Home },
+      { path: 'products', component: Products },
+      { path: 'contact', component: Contact },
+];
