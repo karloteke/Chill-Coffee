f2ce3e2a0c6d7f47da54dc0e446f15aa5fa3253c
Carlota Cetina <carlotika26@gmail.com>
2025-10-28 01:45:35 +0100

    Changes design header and footer

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,10 +3,15 @@ import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
 })
 export class Header {
+  menuAbierto = false;
 
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
 }
