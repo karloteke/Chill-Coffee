3ecdf0fe60a89ac0870544669f6a7edf8d12f24a
Carlota Cetina <carlotika26@gmail.com>
2025-11-25 23:47:40 +0100

    Add contact section and improve typography

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,30 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css']
 })
+
 export class ContactComponent {
-  contact = {
-    name: '',
-    email: '',
-    message: ''
-  };
-
-  submitted = false;
-
-  onSubmit() {
-    if (this.contact.name && this.contact.email && this.contact.message) {
-      this.submitted = true;
-      console.log('Formulario enviado:', this.contact);
-      // Aquí podrías conectar con un servicio o API más adelante
-      this.contact = { name: '', email: '', message: '' };
-    } else {
-      alert('Por favor, completa todos los campos.');
+  onSubmit(form: NgForm){
+    if(!form.valid){
+      return;
     }
+    const payload = form.value;
+    console.log('Enviado:', payload);
+    alert('¡Gracias! Te contactaremos pronto.');
+    form.resetForm();
   }
 }
+
+  // submitted = false;
+
+  // onSubmit() {
+  //   if (this.contact.name && this.contact.email && this.contact.message) {
+  //     this.submitted = true;
+  //     console.log('Formulario enviado:', this.contact);
+  //     // Aquí podrías conectar con un servicio o API más adelante
+  //     this.contact = { name: '', email: '', message: '' };
+  //   } else {
+  //     alert('Por favor, completa todos los campos.');
+  //   }
+  // }
+
